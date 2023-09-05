18abd236f08da6c3621f9dae4ae2b4fb708977f4
karis-m <moses.kamau@student.moringaschool.com>
2023-09-05 04:13:53 -0400

    Updated

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
+class Anagram:
+    list = []
+    def __init__(self, word):
+        self.word = word
+        self.list.append(word)
+        
+    
+    def match(self):
+        pass
+
+    def __repr__(self) -> str:
+        return f'{self.list}'
+
 # your code goes here!
